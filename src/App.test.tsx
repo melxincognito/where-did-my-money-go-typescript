a1fe5b98b0a30513267b1740a049afd6fee40ce8
Mel Incognito <melxincognito@gmail.com>
2022-08-24 19:25:46 -0700

    added test for PurchaseTile element

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,37 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { PurchaseTile } from "./components/PurchaseTile";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Purchase Tile", () => {
+  const deletePurchase = (
+    id: string,
+    amount: number,
+    isNecessity: boolean,
+    purchaseCategory: string
+  ) => {
+    return;
+  };
+
+  it("should render the purchase information", () => {
+    render(
+      <PurchaseTile
+        id="1"
+        name="Movie Tickets"
+        amount={420.22}
+        isNecessity={false}
+        category="Entertainment"
+        deletePurchase={() =>
+          deletePurchase("1", 420.22, true, "Entertainment")
+        }
+      />
+    );
+    const purchaseNameElement = screen.getByText(/movie tickets/i);
+    expect(purchaseNameElement).toBeInTheDocument();
+
+    const purchaseAmountElement = screen.getByText(/420.22/i);
+    expect(purchaseAmountElement).toBeInTheDocument();
+
+    const isNecessityElement = screen.getByText(/wants purchase/i);
+    expect(isNecessityElement).toBeInTheDocument();
+  });
 });
