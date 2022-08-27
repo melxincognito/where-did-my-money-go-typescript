dbed0d3528e29e41b8c2a256149717d02117e201
Mel Incognito <melxincognito@gmail.com>
2022-08-26 18:47:18 -0700

    added chartspage route

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { Layout } from "./components/navigation/Layout";
 import { Routes, Route } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
+import { ChartsPage } from "./pages/ChartsPage";
 
 const App: FC = () => {
   return (
@@ -10,6 +11,7 @@ const App: FC = () => {
       <Layout>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/charts" element={<ChartsPage />} />
         </Routes>
       </Layout>
     </div>
