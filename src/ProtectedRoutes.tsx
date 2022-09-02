745d5da0d1500f6678f1bf8fc47215476f4b5ab7
Mel Incognito <melxincognito@gmail.com>
2022-09-01 18:43:26 -0700

    added return types to protexted routes

--- src/ProtectedRoutes.tsx
+++ src/ProtectedRoutes.tsx
@@ -2,12 +2,12 @@ import { LoginPage } from "./pages/LoginPage";
 import { Outlet } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 
-function CheckUserAuthentication() {
+function CheckUserAuthentication(): boolean {
   const { isAuthenticated } = useAuth0();
   return isAuthenticated;
 }
 
-export default function ProtectedRoutes() {
-  const isUserAuthenticated = CheckUserAuthentication();
+export default function ProtectedRoutes(): JSX.Element {
+  const isUserAuthenticated: boolean = CheckUserAuthentication();
   return isUserAuthenticated ? <Outlet /> : <LoginPage />;
 }
