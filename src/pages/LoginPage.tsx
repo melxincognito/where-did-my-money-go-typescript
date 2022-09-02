a0a9e986e86f115ca207d58fcece78a43ab601b2
Mel Incognito <melxincognito@gmail.com>
2022-09-01 18:39:09 -0700

    removed spacing

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -5,12 +5,14 @@ import { UserProfile } from "../components/authentication/UserProfile";
 export const LoginPage: FC = () => {
   const { loginWithRedirect } = useAuth0();
   const { logout } = useAuth0();
+  const { isAuthenticated } = useAuth0();
   return (
     <div>
       <button onClick={() => loginWithRedirect()}> Log in</button>
       <button onClick={() => logout({ returnTo: window.location.origin })}>
         Log Out
       </button>
+      <button onClick={() => console.log(isAuthenticated)}>auth</button>
       <UserProfile />
     </div>
   );
