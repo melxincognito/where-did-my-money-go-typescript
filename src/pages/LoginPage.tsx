64406a6410137ae3175cc993b6a68cd5336bec31
Mel Incognito <melxincognito@gmail.com>
2022-09-01 22:40:50 -0700

    extremely basic login page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,18 +1,39 @@
 import { FC } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { UserProfile } from "../components/authentication/UserProfile";
+import { Button, Card } from "@mui/material";
 
 export const LoginPage: FC = (): JSX.Element => {
-  const { loginWithRedirect } = useAuth0();
-  const { logout } = useAuth0();
-  const { isAuthenticated } = useAuth0();
+  const { loginWithPopup } = useAuth0();
+
   return (
-    <div>
-      <button onClick={() => loginWithRedirect()}> Log in</button>
-      <button onClick={() => logout({ returnTo: window.location.origin })}>
-        Log Out
-      </button>
-      <button onClick={() => console.log(isAuthenticated)}>auth</button>
+    <div style={{ display: "grid", justifyItems: "center" }}>
+      <Card
+        sx={{
+          width: "90%",
+          height: "4rem",
+          borderRadius: "20px",
+          boxShadow: "0px 0px 15px 5px rgba(0,0,0,0.38)",
+        }}
+      >
+        <Button
+          sx={{
+            width: "100%",
+            height: "100%",
+            fontSize: "1.5rem",
+            borderRadius: "20px",
+            backgroundColor: "secondary.main",
+            color: "primary.main",
+            "&:hover": {
+              backgroundColor: "secondary.dark",
+            },
+          }}
+          variant="contained"
+          onClick={() => loginWithPopup()}
+        >
+          Login / Sign Up
+        </Button>
+      </Card>
       <UserProfile />
     </div>
   );
