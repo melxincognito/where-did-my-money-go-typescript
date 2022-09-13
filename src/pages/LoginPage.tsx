c840587d86ccb97b52a9b95ddfd3cdc826baf878
Mel Incognito <melxincognito@gmail.com>
2022-09-12 22:03:00 -0700

    added supabase login with google and github providers

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,44 +1,66 @@
-import { FC } from "react";
-import { useAuth0 } from "@auth0/auth0-react";
-import { UserProfile } from "../components/authentication/UserProfile";
 import { Button, Card } from "@mui/material";
+import { supabase } from "../supabaseClient";
 
-import { ImageCarousel } from "../components/login/ImageCarousel";
+export const LoginPage = () => {
+  async function signInWithGithub() {
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: "github",
+    });
+    if (error) alert("Error: " + error);
+  }
 
-export const LoginPage: FC = (): JSX.Element => {
-  const { loginWithRedirect } = useAuth0();
+  async function signInWithGoogle() {
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: "google",
+    });
+    if (error) alert("Error: " + error);
+  }
 
   return (
-    <div style={{ display: "grid", justifyItems: "center" }}>
-      <ImageCarousel />
-
-      <Card
-        sx={{
-          width: "90%",
-          height: "4rem",
-          borderRadius: "20px",
-          boxShadow: "0px 0px 15px 5px rgba(0,0,0,0.38)",
-        }}
-      >
+    <div style={styles.container}>
+      <Card style={styles.card}>
         <Button
-          sx={{
-            width: "100%",
-            height: "100%",
-            fontSize: "1.5rem",
-            borderRadius: "20px",
-            backgroundColor: "secondary.main",
-            color: "primary.main",
-            "&:hover": {
-              backgroundColor: "secondary.dark",
-            },
-          }}
+          sx={styles.button}
           variant="contained"
-          onClick={() => loginWithRedirect()}
+          onClick={signInWithGoogle}
         >
-          Login / Sign Up
+          Sign in with Google
         </Button>
       </Card>
-      <UserProfile />
+      <Card sx={styles.card}>
+        <Button
+          sx={styles.button}
+          variant="contained"
+          onClick={signInWithGithub}
+        >
+          Sign in with Github
+        </Button>
+      </Card>{" "}
     </div>
   );
 };
+
+const styles = {
+  container: {
+    display: "grid",
+    justifyItems: "center",
+    gap: "2rem",
+  },
+  card: {
+    width: "90%",
+    height: "4rem",
+    borderRadius: "20px",
+    boxShadow: "0px 0px 15px 5px rgba(0,0,0,0.38)",
+  },
+  button: {
+    width: "100%",
+    height: "100%",
+    fontSize: "1.5rem",
+    borderRadius: "20px",
+    backgroundColor: "secondary.main",
+    color: "primary.main",
+    "&:hover": {
+      backgroundColor: "secondary.dark",
+    },
+  },
+} as const;
